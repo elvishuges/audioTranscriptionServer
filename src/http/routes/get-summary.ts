25f42fad269be5b424e9550c251993b104289b62
Elvis HGS <elvishuges@hotmail.com>
2025-07-24 20:23:26 -0300

    fix: query get summary from room. returning correct summary

--- src/http/routes/get-summary.ts
+++ src/http/routes/get-summary.ts
@@ -1,20 +1,32 @@
 import { count, eq } from "drizzle-orm";
+import { z } from "zod/v4";
 import type { FastifyPluginCallbackZod } from "fastify-type-provider-zod";
 import { db } from "../../db/connection.ts";
 import { schema } from "../../db/schema/index.ts";
 
 export const getSummaryRoute: FastifyPluginCallbackZod = (app) => {
-  app.get("/rooms/:roomId/sumarry", async () => {
-    const results = await db
-      .select({
-        id: schema.sumarryChunks.id,
-        content: schema.sumarryChunks.content,
-        createdAt: schema.sumarryChunks.createdAt,
-      })
-      .from(schema.sumarryChunks)
-      .leftJoin(schema.rooms, eq(schema.sumarryChunks.roomId, schema.rooms.id))
-      .limit(1);
+  app.get(
+    "/rooms/:roomId/sumarry",
+    {
+      schema: {
+        params: z.object({
+          roomId: z.string(),
+        }),
+      },
+    },
+    async (request) => {
+      const { roomId } = request.params;
+      const results = await db
+        .select({
+          id: schema.sumarryChunks.id,
+          content: schema.sumarryChunks.content,
+          createdAt: schema.sumarryChunks.createdAt,
+        })
+        .from(schema.sumarryChunks)
+        .where(eq(schema.sumarryChunks.roomId, roomId))
+        .limit(1);
 
-    return results.at(0) ?? null;
-  });
+      return results.at(0) ?? null;
+    }
+  );
 };
